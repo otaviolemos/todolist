888c7ffcad467ec4e288c9dc742e0f1edcd8b18c
Otavio Lemos <otaviolemos@gmail.com>
2022-10-24 20:47:15 -0300

    fix: remove files from root

--- priority.py
+++
@@ -1,5 +0,0 @@
-from enum import Enum
-class Priority(Enum):
-     HIGH = 0
-     MEDIUM = 1
-     LOW = 2
--- user.py
+++
@@ -1,4 +0,0 @@
-class User:
-    def __init__(self, name, email):
-        self.name = name
-        self.email = email
