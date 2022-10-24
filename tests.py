4114b0e4df2c6bc1e3160c142038cc0f148ba66d
Otavio Lemos <otaviolemos@gmail.com>
2022-10-24 14:19:05 -0300

    test: add priority

--- tests.py
+++ tests.py
@@ -1,27 +1,28 @@
 from todoitem import TodoItem
 from user import User
 from todolist import TodoList
+from priority import Priority
 
 def test_done():
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     item.complete()
     assert item.is_done() == True
 
 def test_undone():
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     item.complete()
     item.undo()
     assert item.is_done() == False
 
 def test_change_description():
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     item.change_description('change bed')
     assert item.description == 'change bed'
 
 def test_one_todo_list():
     owner = User('Joe Doe', 'joe@doe.com')
     list = TodoList(owner)
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     list.add(item)
     assert list.get(0) == item
     assert list.get_owner() == owner
@@ -29,7 +30,7 @@ def test_one_todo_list():
 def test_complete_item_from_todo_list():
     owner = User('Joe Doe', 'joe@doe.com')
     list = TodoList(owner)
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     list.add(item)
     list.complete_item(0)
     assert item.is_done() == True
@@ -37,7 +38,7 @@ def test_complete_item_from_todo_list():
 def test_remove_item_from_todo_list():
     owner = User('Joe Doe', 'joe@doe.com')
     list = TodoList(owner)
-    item = TodoItem('make bed')
+    item = TodoItem('make bed', Priority.LOW)
     list.add(item)
     list.remove(0)
     assert list.size() == 0
@@ -45,8 +46,36 @@ def test_remove_item_from_todo_list():
 def test_search_item_by_description():
     owner = User('Joe Doe', 'joe@doe.com')
     list = TodoList(owner)
-    item1 = TodoItem('make bed')
-    item2 = TodoItem('withdraw cash')
+    item1 = TodoItem('make bed', Priority.LOW)
+    item2 = TodoItem('withdraw cash', Priority.LOW)
     list.add(item1)
     list.add(item2)
     assert list.find('withdraw cash') == item2
+
+def test_items_sorted_by_priority():
+    item1 = TodoItem('make bed', Priority.LOW)
+    item2 = TodoItem('withdraw cash', Priority.MEDIUM)
+    item3 = TodoItem('call mom', Priority.HIGH)
+    owner = User('Joe Doe', 'joe@doe.com')
+    list = TodoList(owner)
+    list.add(item1)
+    list.add(item2)
+    list.add(item3)
+    assert list.get(0).description == item3.description
+    assert list.get(1).description == item2.description
+    assert list.get(2).description == item1.description
+
+def test_change_priority_resorts():
+    item1 = TodoItem('make bed', Priority.LOW)
+    item2 = TodoItem('withdraw cash', Priority.MEDIUM)
+    item3 = TodoItem('call mom', Priority.HIGH)
+    owner = User('Joe Doe', 'joe@doe.com')
+    list = TodoList(owner)
+    list.add(item1)
+    list.add(item2)
+    list.add(item3)
+    list.change_priority(0, Priority.LOW)
+    list.change_priority(2, Priority.HIGH)
+    assert list.get(0).description == item1.description
+    assert list.get(1).description == item2.description
+    assert list.get(2).description == item3.description
