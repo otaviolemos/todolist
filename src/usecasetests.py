233d3093d57b867a77487c9c506232a2837f168e
Otavio Lemos <otaviolemos@gmail.com>
2022-10-31 20:00:07 -0300

    test: hash password

--- src/usecasetests.py
+++ src/usecasetests.py
@@ -2,22 +2,25 @@ from inmemoryuserrepository import InMemoryUserRepository
 from signup import SignUp
 import pytest
 from duplicateusererror import DuplicateUserError
+from fakehashservice import FakeHashService
 
 def test_signup_with_valid_data():
     user_repo = InMemoryUserRepository()
+    hash_service = FakeHashService()
     user_name = 'Joe Doe'
     user_email = 'joe@doe.com'
     user_password = 'test1234TEST&'
-    usecase = SignUp(user_repo)
+    usecase = SignUp(user_repo, hash_service)
     usecase.perform(user_name, user_email, user_password)
     assert user_repo.find_by_email(user_email).name == user_name
 
 def test_prevent_duplicate_user():
     user_repo = InMemoryUserRepository()
+    hash_service = FakeHashService()
     user_name = 'Joe Doe'
     user_email = 'joe@doe.com'
     user_password = 'test1234TEST&'
-    usecase = SignUp(user_repo)
+    usecase = SignUp(user_repo, hash_service)
     usecase.perform(user_name, user_email, user_password)
     dup_user_name = 'Doe Joe'
     dup_user_email = 'joe@doe.com'
@@ -25,4 +28,15 @@ def test_prevent_duplicate_user():
     with pytest.raises(DuplicateUserError):
         usecase.perform(dup_user_name, dup_user_email, dup_user_password)
 
-
+def test_hash_password():
+    user_repo = InMemoryUserRepository()
+    hash_service = BcryptHashService(bcrypt.gensalt(14))
+    user_name = 'Joe Doe'
+    user_email = 'joe@doe.com'
+    user_password = 'test1234TEST&'
+    usecase = SignUp(user_repo, hash_service)
+    usecase.perform(user_name, user_email, user_password)
+    saved_user = user_repo.find_by_email(user_email)
+    assert saved_user.name == user_name
+    assert saved_user.password != user_password
+    assert hash_service.check(user_password, saved_user.password)
