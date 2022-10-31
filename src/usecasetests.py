afd78c18f9af2a6938db81a66733cf7386f93f9e
Otavio Lemos <otaviolemos@gmail.com>
2022-10-31 19:21:12 -0300

    test: duplicate user

--- src/usecasetests.py
+++ src/usecasetests.py
@@ -1,5 +1,7 @@
 from inmemoryuserrepository import InMemoryUserRepository
 from signup import SignUp
+import pytest
+from duplicateusererror import DuplicateUserError
 
 def test_signup_with_valid_data():
     user_repo = InMemoryUserRepository()
@@ -9,3 +11,18 @@ def test_signup_with_valid_data():
     usecase = SignUp(user_repo)
     usecase.perform(user_name, user_email, user_password)
     assert user_repo.find_by_email(user_email).name == user_name
+
+def test_prevent_duplicate_user():
+    user_repo = InMemoryUserRepository()
+    user_name = 'Joe Doe'
+    user_email = 'joe@doe.com'
+    user_password = 'test1234TEST&'
+    usecase = SignUp(user_repo)
+    usecase.perform(user_name, user_email, user_password)
+    dup_user_name = 'Doe Joe'
+    dup_user_email = 'joe@doe.com'
+    dup_user_password = 'TEST1234test&'
+    with pytest.raises(DuplicateUserError):
+        usecase.perform(dup_user_name, dup_user_email, dup_user_password)
+
+
