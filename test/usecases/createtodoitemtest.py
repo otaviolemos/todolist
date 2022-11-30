d12cb919d17e6ec9a588c5ed808586cbd0f51ee2
Otavio Lemos <otaviolemos@gmail.com>
2022-11-30 20:19:07 -0300

    test: duplicate item in usecase

--- test/usecases/createtodoitemtest.py
+++ test/usecases/createtodoitemtest.py
@@ -8,6 +8,7 @@ from src.usecases.errors.invalidpassworderror import InvalidPasswordError
 from src.usecases.errors.invalidcredentialserror import InvalidCredentialsError
 from src.usecases.errors.invalidusererror import InvalidUserError
 from src.usecases.errors.duplicatetodolisterror import DuplicateTodoListError
+from src.entities.errors.duplicateitemerror import DuplicateItemError
 from test.usecases.fakehashservice import FakeHashService
 from test.usecases.inmemorytodolistrepository import InMemoryTodoListRepository
 from test.usecases.inmemoryuserrepository import InMemoryUserRepository
@@ -28,3 +29,19 @@ def test_create_item_in_todo_list():
     persisted_todo_list = todolist_repo.find_by_email(user_email)
     assert persisted_todo_list.size() == 1
     assert persisted_todo_list.get(0).description == item_description
+
+def test_create_duplicate_item_in_todo_list():
+    user_repo = InMemoryUserRepository()
+    todolist_repo = InMemoryTodoListRepository()
+    hash_service = FakeHashService()
+    user_name = 'Joe Doe'
+    user_email = 'joe@doe.com'
+    user_password = 'test1234TEST&'
+    SignUp(user_repo, hash_service).perform(user_name, user_email, user_password)
+    CreateTodoList(user_repo, todolist_repo).perform(user_email)
+    usecase = CreateTodoItem(user_repo, todolist_repo)
+    item_description = 'call mom'
+    item_priority = 0
+    usecase.perform(user_email, item_description, item_priority)
+    with pytest.raises(DuplicateItemError):
+        usecase.perform(user_email, item_description, item_priority)
