7c1687c5a5e73317ea196b222a00820567c03327
Otavio Lemos <otaviolemos@gmail.com>
2022-12-05 14:12:53 -0300

    feat: handle invalid user

--- src/usecases/gettodolist.py
+++ src/usecases/gettodolist.py
@@ -1,6 +1,12 @@
+from src.usecases.errors.invalidusererror import InvalidUserError
+
+
 class GetTodoList():
     def __init__(self, todolist_repository):
         self.todolist_repository = todolist_repository
 
     def perform(self, user_email):
-        return self.todolist_repository.find_by_email(user_email)
+        todolist = self.todolist_repository.find_by_email(user_email)
+        if not todolist:
+            raise InvalidUserError()
+        return todolist
