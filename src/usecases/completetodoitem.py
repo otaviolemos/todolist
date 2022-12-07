d76c6ce29c815376c04eb88462a8715193a2069f
Otavio Lemos <otaviolemos@gmail.com>
2022-12-07 14:32:34 -0300

    feat: complete item use case

--- src/usecases/completetodoitem.py
+++ src/usecases/completetodoitem.py
@@ -1,8 +1,13 @@
+from src.usecases.errors.invalidusererror import InvalidUserError
+
+
 class CompleteTodoItem():
     def __init__(self, todolist_repo):
         self.todolist_repo = todolist_repo
 
     def perform(self, user_email, item_description):
         todolist = self.todolist_repo.find_by_email(user_email)
+        if not todolist:
+            raise InvalidUserError()
         todolist.complete_by_description(item_description)
         self.todolist_repo.update(user_email, todolist)
