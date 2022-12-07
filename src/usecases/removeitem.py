3549624e9367401ff3375f1f3d5aab8d58b36a3a
Otavio Lemos <otaviolemos@gmail.com>
2022-12-07 14:20:05 -0300

    feat: handle invalid user

--- src/usecases/removeitem.py
+++ src/usecases/removeitem.py
@@ -1,8 +1,13 @@
+from src.usecases.errors.invalidusererror import InvalidUserError
+
+
 class RemoveItem():
     def __init__(self, todolist_repo):
         self.todolist_repo = todolist_repo
 
     def perform(self, user_email, item_description):
         todolist = self.todolist_repo.find_by_email(user_email)
+        if not todolist:
+            raise InvalidUserError()
         todolist.remove_by_description(item_description)
         self.todolist_repo.update(user_email, todolist)
