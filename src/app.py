b35c37637649ee60575376e8b064f351044bca96
Otavio Lemos <otaviolemos@gmail.com>
2022-12-12 15:06:38 -0300

    feat: move external service creation and use try in controller

--- src/app.py
+++ src/app.py
@@ -8,17 +8,22 @@ from src.usecases.signup import SignUp
 
 def create_app():
     app = Flask(__name__)
+    salt = bcrypt.gensalt()
+    user_repo = InMemoryUserRepository()
+    hash_service = BcryptHashService(salt)
 
     @app.route("/api/users", methods = ['POST'])
     def signup():
         name = request.json["name"]
         email = request.json["email"]
         password = request.json["password"]
-        salt = bcrypt.gensalt()
-        hash_service = BcryptHashService(salt)
-        user_repo = InMemoryUserRepository()
         signup_usecase = SignUp(user_repo, hash_service)
-        signup_usecase.perform(name, email, password)
+        try:
+          signup_usecase.perform(name, email, password)
+        except Exception as error:
+          return jsonify({
+            "error": str(error)
+          }), 400
         result = { 
           "name": name,
           "email": email 
