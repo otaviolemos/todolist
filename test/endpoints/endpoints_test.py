d4e397b52ba89b1d66d6c91bdb7549547133e97a
Otavio Lemos <otaviolemos@gmail.com>
2022-12-12 15:07:53 -0300

    test: signup with error

--- test/endpoints/endpoints_test.py
+++ test/endpoints/endpoints_test.py
@@ -23,3 +23,16 @@ def test_user_signup(client):
     assert response.status_code == 201
     assert response.json["name"] is not None
     assert response.json["email"] is not None
+
+def test_user_signup_with_error(client):
+    user_name = 'Joe Doe'
+    user_email = 'joe@doe.com'
+    user_password = 'test1234TEST'
+    response = client.post('/api/users', json=
+    {
+      "name": user_name,
+      "email": user_email,
+      "password": user_password
+    })
+    assert response.status_code == 400
+    assert response.json["error"] is not None
