58f54d974ecf62608c1832d7350d220f1d40240b
Otavio Lemos <otaviolemos@gmail.com>
2022-12-18 18:52:03 -0300

    test: invalid user or no todo list

--- test/usecases/removetodolist_test.py
+++ test/usecases/removetodolist_test.py
@@ -1,4 +1,7 @@
+import pytest
 from src.usecases.createtodolist import CreateTodoList
+from src.usecases.errors.invalidusererror import InvalidUserError
+from src.usecases.errors.invaliduserotnotodolist import InvalidUserOrNoTodoList
 from src.usecases.removetodolist import RemoveTodoList
 from src.usecases.signup import SignUp
 from test.usecases.fakehashservice import FakeHashService
@@ -19,3 +22,14 @@ def test_remove_todolist():
     usecase.perform(user_email)
     no_todo_list = todolist_repo.find_by_email(user_email)
     assert no_todo_list is None
+
+def test_remove_todolist_from_user_without_todolist():
+    todolist_repo = InMemoryTodoListRepository()
+    user_repo = InMemoryUserRepository()
+    user_email = 'joe@doe.com'
+    user_name = 'Joe Doe'
+    user_password = 'test1234TEST&'
+    SignUp(user_repo, FakeHashService()).perform(user_name, user_email, user_password)
+    usecase = RemoveTodoList(todolist_repo)
+    with pytest.raises(InvalidUserOrNoTodoList):
+        usecase.perform(user_email)
